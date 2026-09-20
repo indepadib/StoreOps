c8636151251a99554d11bddd07e7fbade4e8b339
indepadib <55211714+indepadib@users.noreply.github.com>
2026-09-20 21:53:56 +0100

    chore(v2.18): refresh frontend/runtime-config.js

--- frontend/runtime-config.js
+++ frontend/runtime-config.js
@@ -1,2 +1,2 @@
 window.STOREOPS_CONFIG=window.STOREOPS_CONFIG||{apiBase:'',mode:'showcase',entra:{tenantId:'',spaClientId:'',apiScope:''}};
-window.STOREOPS_BUILD=window.STOREOPS_BUILD||'2.16.0';
+window.STOREOPS_BUILD=window.STOREOPS_BUILD||'2.18.0';
